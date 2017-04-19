9d8f4563c0449bc6731a79f217ccd03a37b68b80
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-18 20:29:27 -0500

    ready with comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
 //document.body.innerHTML = "hola";
 var body  = document.getElementsByTagName("BODY")[0];
-//var div  = document.createElement("div");
-//document.body.appendChild(div);
+// Creo la variable para trabajar dentro del body
 var redDiv = document.createElement("div");
     redDiv.setAttribute("id","red");
     document.body.appendChild(redDiv);
-
+// Creo el div rojo con su id
 var purpleDiv = document.createElement("div");
     purpleDiv.setAttribute("id","purple");
     document.body.appendChild(purpleDiv);
@@ -15,11 +14,11 @@ var yellowDiv = document.createElement("div");
     yellowDiv.setAttribute("id","yellow");
     document.body.appendChild(yellowDiv);
     document.getElementById("red").appendChild(yellowDiv);
-
+// Creo los divs morado y amarillo dentro del div rojo
 var blueDiv = document.createElement("div");
     blueDiv.setAttribute("id","blue");
     document.body.appendChild(blueDiv);
-
+// Creo el div azul donde iran los divs verde y negro
 var greenDiv = document.createElement("div");
     greenDiv.setAttribute("id","green");
     document.body.appendChild(greenDiv);
