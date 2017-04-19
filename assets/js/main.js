753e2c3ef614dacdd61319f6bd7741fb54d5ca48
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-18 20:10:21 -0500

    aligning

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,23 @@ var redDiv = document.createElement("div");
 var purpleDiv = document.createElement("div");
     purpleDiv.setAttribute("id","purple");
     document.body.appendChild(purpleDiv);
+    document.getElementById("red").appendChild(purpleDiv);
 
 var yellowDiv = document.createElement("div");
     yellowDiv.setAttribute("id","yellow");
     document.body.appendChild(yellowDiv);
+    document.getElementById("red").appendChild(yellowDiv);
 
-var blackDiv = document.createElement("div");
-    blackDiv.setAttribute("id","black");
-    document.body.appendChild(blackDiv);
+var blueDiv = document.createElement("div");
+    blueDiv.setAttribute("id","blue");
+    document.body.appendChild(blueDiv);
 
 var greenDiv = document.createElement("div");
     greenDiv.setAttribute("id","green");
     document.body.appendChild(greenDiv);
+    document.getElementById("blue").appendChild(greenDiv);
 
-var blueDiv = document.createElement("div");
-  	blueDiv.setAttribute("id","blue");
-    document.body.appendChild(blueDiv);
+var blackDiv = document.createElement("div");
+    blackDiv.setAttribute("id","black");
+    document.body.appendChild(blackDiv);
+    document.getElementById("blue").appendChild(blackDiv);
